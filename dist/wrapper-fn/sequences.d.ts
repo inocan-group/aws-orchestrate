1a8156556565f9da5574914d9e506b7f12f32d13
Ken Snyder <ken@ken.net>
2019-09-14 10:36:49 -0700

    removed circular deps coming from aws-log

--- dist/wrapper-fn/sequences.d.ts
+++ dist/wrapper-fn/sequences.d.ts
@@ -7,4 +7,4 @@ import { IAWSLambaContext } from "common-types";
 export declare function registerSequence(log: ILoggerApi, context: IAWSLambaContext): (s: LambdaSequence) => void;
 /** returns the sequence which was set by `startSequence()` */
 export declare function getNewSequence(): LambdaSequence;
-export declare function invokeNewSequence(results: any, log: ILoggerApi): Promise<unknown>;
+export declare function invokeNewSequence(results: any, log: ILoggerApi): Promise<import("aws-sdk/clients/lambda").InvocationResponse>;
