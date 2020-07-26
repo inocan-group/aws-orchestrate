a01933c2e972db5daf8137471d7629dc669152cf
Ken Snyder <ken@ken.net>
2020-07-25 21:52:07 -0700

    remove dist folder from repo

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
-exports.default = {
+module.exports = {
   roots: ["<rootDir>/test"],
-  testMatch: ["**/?(*[.-])+(spec|test).+(ts|tsx|js)"],
+  testMatch: ["**/*-spec.ts"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "(firemodel|universal-fire|@forest-fire|lodash-es).+\\.js$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(firemodel|universal-fire|@forest-fire|lodash-es)).+\\.js$"],
+  moduleNameMapper: {
+    "^firemodel$": "<rootDir>/node_modules/firemodel/dist/es/index.js",
+    "^universal-fire$": "<rootDir>/node_modules/universal-fire/dist/es/index.js",
+    "^lodash-es$": "<rootDir>/node_modules/lodash/index.js",
+    // '^@forest-fire/firestore-client$': '<rootDir>/node_modules/@forest-fire/firestore-client/dist/es/index.js',
+    // '^@forest-fire/real-time-client$': '<rootDir>/node_modules/@forest-fire/real-time-client/dist/es/index.js',
   },
 };
--- test/handled-error-spec.ts
+++ test/handled-error-spec.ts
@@ -1,3 +1,5 @@
+import * as helpers from "./testing/helpers";
+
 import { IAWSLambaContext, IAWSLambdaProxyIntegrationRequest } from "common-types";
 import { IHandlerContext, IHandlerFunction, ServerlessError, wrapper } from "../src/index";
 
@@ -7,29 +9,28 @@ const FUNCTION_NAME = "myHandlerFunction";
 const ERROR_CODE = 403;
 
 describe("Handling errors => ", () => {
-  it(
-    "throwing a ServerlessError is passed through by the wrapper function",
-    async () => {
-      let foo: number = 0;
-      const myHandler: IHandlerFunction<void, void> = async (req, ctx) => {
-        foo = 1;
-        throw new ServerlessError(ERROR_CODE, "a test of an explicit error throw", "testing");
-      };
-      try {
-        const wrapped = wrapper(myHandler);
-        const result = await wrapped(
-          { headers: { "X-Correlation-Id": CORRELATION_ID } } as IAWSLambdaProxyIntegrationRequest,
-          {
-            awsRequestId: AWS_REQUEST_ID,
-            functionName: FUNCTION_NAME,
-          } as IAWSLambaContext
-        );
-      } catch (e) {
-        expect(e.name).toBe("ServerlessError");
-        expect(e.httpStatus).toBe(ERROR_CODE);
-        expect(e.correlationId).toBe(CORRELATION_ID);
-        expect(e.awsRequestId).toBe(AWS_REQUEST_ID);
-      }
+  it("throwing a ServerlessError is passed through by the wrapper function", async () => {
+    let foo: number = 0;
+    const myHandler: IHandlerFunction<void, void> = async (req, ctx) => {
+      foo = 1;
+      throw new ServerlessError(ERROR_CODE, "a test of an explicit error throw", "testing");
+    };
+    try {
+      const restore = helpers.captureStdout();
+      const wrapped = wrapper(myHandler);
+      restore();
+      const result = await wrapped(
+        { headers: { "X-Correlation-Id": CORRELATION_ID } } as IAWSLambdaProxyIntegrationRequest,
+        {
+          awsRequestId: AWS_REQUEST_ID,
+          functionName: FUNCTION_NAME,
+        } as IAWSLambaContext
+      );
+    } catch (e) {
+      expect(e.name).toBe("ServerlessError");
+      expect(e.httpStatus).toBe(ERROR_CODE);
+      expect(e.correlationId).toBe(CORRELATION_ID);
+      expect(e.awsRequestId).toBe(AWS_REQUEST_ID);
     }
-  );
+  });
 });
